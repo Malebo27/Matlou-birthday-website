bb85c257173fbbc17e1f6708d2a4cc50a1562a56
Malebo Sibanda <maleboroslyn212@gmail.com>
2026-01-29 15:42:16 +0200

    Initial commit

---
+++ Birthday-website/script.js
@@ -0,0 +1,80 @@
+const music = document.getElementById("bg-music");
+const btn = document.getElementById("music-btn");
+
+let isPlaying = false;
+
+btn.addEventListener("click", () => {
+  if (!isPlaying) {
+    music.play();
+    btn.textContent = "⏸ Pause Music";
+  } else {
+    music.pause();
+    btn.textContent = "▶ Play Music";
+  }
+  isPlaying = !isPlaying;
+});
+
+const images = document.querySelectorAll(".photo img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.getElementById("close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+lightboxImg.src = img.src;
+
+// heart burst 💕
+for (let i = 0; i < 8; i++) {
+  setTimeout(createHeart, i * 120);
+}
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== lightboxImg) {
+    lightbox.style.display = "none";
+  }
+});
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️";
+
+  heart.style.left = Math.random() * window.innerWidth + "px";
+  heart.style.top = (window.innerHeight / 2) + "px";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 2000);
+}
+
+const giftBtn = document.getElementById("gift-btn");
+const giftModal = document.getElementById("gift-modal");
+const giftClose = document.getElementById("gift-close");
+
+giftBtn.addEventListener("click", () => {
+  giftModal.style.display = "flex";
+
+  // hearts when gift opens 💕
+  for (let i = 0; i < 10; i++) {
+    setTimeout(createHeart, i * 100);
+  }
+});
+
+giftClose.addEventListener("click", () => {
+  giftModal.style.display = "none";
+});
+
+giftModal.addEventListener("click", (e) => {
+  if (e.target === giftModal) {
+    giftModal.style.display = "none";
+  }
+});
